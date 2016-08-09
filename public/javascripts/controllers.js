43ec2c9bb6fac5897c31974d80716af07ff1f6b3
naoki kodama <naoki.koda@gmail.com>
2016-08-09 20:37:38 +0900

    リスト表示、保存を実装

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,20 +1,46 @@
 // Managing the poll list
-function PollListCtrl($scope) {
-  $scope.polls = [];
+function PollListCtrl($scope, Poll) {
+  $scope.polls = Poll.query();
 }
 // Voting / viewing poll results
-function PollItemCtrl($scope, $routeParams) {
-  $scope.poll = {};
+function PollItemCtrl($scope, $routeParams, Poll) {
+  $scope.poll = Poll.get({pollId: $routeParams.pollId});
   $scope.vote = function() {};
 }
+
 // Creating a new poll
-function PollNewCtrl($scope) {
-  $scope.poll = {
-    question: '',
-    choices: [{ text: '' }, { text: '' }, { text: '' }]
-  };
-  $scope.addChoice = function() {
-    $scope.poll.choices.push({ text: '' });
-  };
-  $scope.createPoll = function() {};
+function PollNewCtrl($scope, $location, Poll) {
+	$scope.poll = {
+		question: '',
+		choices: [ { text: '' }, { text: '' }, { text: '' }]
+	};
+	$scope.addChoice = function() {
+		$scope.poll.choices.push({ text: '' });
+	};
+	$scope.createPoll = function() {
+		var poll = $scope.poll;
+		if(poll.question.length > 0) {
+			var choiceCount = 0;
+			for(var i = 0, ln = poll.choices.length; i < ln; i++) {
+				var choice = poll.choices[i];
+				if(choice.text.length > 0) {
+					choiceCount++
+				}
+			}
+			if(choiceCount > 1) {
+				var newPoll = new Poll(poll);
+				newPoll.$save(function(p, resp) {
+					if(!p.error) {
+						$location.path('polls');
+					} else {
+						alert('Could not create poll');
+					}
+				});
+			} else {
+				alert('You must enter at least two choices');
+			}
+		} else {
+			alert('You must enter a question');
+		}
+	};
 }
